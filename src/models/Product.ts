e112b044f5bdcf63ca31654d55738f496c390a79
Cody Haines <chaines51@gmail.com>
2021-03-23 19:11:05 -0500

    Updated models to match new tracking method, added functionality for fetching individual reviews, marking reviews helpful, and reporting reviews

--- src/models/Product.ts
+++ src/models/Product.ts
@@ -1,6 +1,12 @@
-import mongoose, { Model, Document } from 'mongoose';
-import { ProductDocument } from '../types';
+import mongoose, { Model } from 'mongoose';
+import { ProductDocument, Review, ReviewDocument } from '../types';
 import productSchema from './schemas/Product';
 
-const Product: Model<ProductDocument> = mongoose.model('Product', productSchema, 'SDCReviews');
+export interface ProductModel extends Model<ProductDocument> {
+  findReview: (reviewId: string) => Promise<ReviewDocument>;
+  markReviewHelpful: (reviewId: string) => Promise<boolean>;
+  reportReview: (reviewId: string) => Promise<boolean>;
+}
+
+const Product = mongoose.model<ProductDocument, ProductModel>('Product', productSchema, 'SDCReviews');
 export default Product;
--- src/models/schemas/Review.ts
+++ src/models/schemas/Review.ts
@@ -1,23 +1,19 @@
 import { Schema } from 'mongoose';
 import photoSchema from './Photo';
 
-const reviewSchema = new Schema(
-  {
-    review_id: Number,
-    rating: Number,
-    summary: String,
-    recommend: Boolean,
-    reported: Boolean,
-    response: String,
-    body: String,
-    date: Date,
-    reviewer_name: String,
-    reviewer_email: String,
-    helpfulness: Number,
-    photos: [photoSchema],
-    characteristics: {},
-  },
-  { _id: false }
-);
+const reviewSchema = new Schema({
+  rating: Number,
+  summary: String,
+  recommend: Boolean,
+  reported: Boolean,
+  response: String,
+  body: String,
+  date: Date,
+  reviewer_name: String,
+  reviewer_email: String,
+  helpfulness: Number,
+  photos: [photoSchema],
+  characteristics: {},
+});
 
 export default reviewSchema;
